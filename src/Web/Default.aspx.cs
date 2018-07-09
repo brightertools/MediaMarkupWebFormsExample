7e30947b77509cae8c10f58050df01a88bcbbb39
Mark Redman <mark@brightertools.com>
2018-07-09 17:19:33 +0100

    Initial Commit

---
+++ src/Web/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.UI;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected  void Page_Load(object sender, EventArgs e)
+    {
+        RegisterAsyncTask(new PageAsyncTask(PageLoadAsync));
+    }
+
+    // Async Page Load
+    public async Task PageLoadAsync()
+    {
+        // Get Client Settings
+        var clientSettings = new MediaMarkup.Settings
+        {
+            ApiBaseUrl = Settings.MediaMarkupBaseUrl,
+            ClientId = Settings.MediaMarkupClientId,
+            SecretKey = Settings.MediaMarkupSecretKey
+        };
+
+        // Create API Client
+        var apiClient = new MediaMarkup.ApiClient(clientSettings);
+
+        // Initialise API Client (Validates api keys and gets an api token for subsequest api calls)
+        var token = await apiClient.InitializeAsync();
+
+        // Check authentication
+        Label1.Text = (await apiClient.Authenticated()).ToString().ToLower() == "true" ? "Yes" : "No";
+    }
+}
